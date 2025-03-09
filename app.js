a52f67728a89f2a438c5150672c7a6ccca58bd1f
oskarziembrowicz <rakson2002@gmail.com>
2025-03-09 15:21:40 +0100

    Handling GET tours/:id request

--- app.js
+++ app.js
@@ -19,6 +19,26 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+
+  const tour = tours.find((el) => el.id == req.params.id * 1);
+
+  if (tour) {
+    res.status(200).json({
+      status: "success",
+      data: {
+        tour,
+      },
+    });
+  } else {
+    res.status(404).json({
+      status: "fail",
+      message: "Invalid ID",
+    });
+  }
+});
+
 app.post("/api/v1/tours", (req, res) => {
   //   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1;
