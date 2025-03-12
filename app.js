2144bd429ac48ef3ce1b7bcfbed105f6ebc00e34
oskarziembrowicz <rakson2002@gmail.com>
2025-03-12 15:25:48 +0100

    Creating simple middleware to test

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ const app = express();
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  console.log("Hello from the middleware 👋");
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
 function getAllTours(req, res) {
+  console.log(req.requestTime);
   res.status(200).json({
     status: "success",
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -96,6 +108,7 @@ function deleteTour(req, res) {
 // app.delete("/api/v1/tours/:id", deleteTour);
 
 app.route("/api/v1/tours").get(getAllTours).post(createTour);
+
 app
   .route("/api/v1/tours/:id")
   .get(getTour)
