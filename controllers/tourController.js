8818e61c2c723b774887eb4d8a19eac379c67920
oskarziembrowicz <rakson2002@gmail.com>
2025-03-13 19:22:15 +0100

    Implementing delete tour with db (CRUD for tours doen)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -83,9 +83,19 @@ exports.updateTour = async (req, res) => {
   }
 };
 
-exports.deleteTour = (req, res) => {
-  res.status(204).json({
-    status: "success",
-    data: null,
-  });
+exports.deleteTour = async (req, res) => {
+  try {
+    // Query the database
+    await Tour.findByIdAndDelete(req.params.id);
+    // Send the response
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: "fail",
+      message: `Error: ${err.message}`,
+    });
+  }
 };
