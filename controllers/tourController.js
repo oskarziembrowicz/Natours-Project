6919441f22e2134b4ba90a65bbd6405f54a7ec95
oskarziembrowicz <rakson2002@gmail.com>
2025-03-15 13:04:03 +0100

    Implementing field select feature to get all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -23,6 +23,14 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
+    // 4) Field limiting
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else {
+      query = query.select("-__v");
+    }
+
     // const tours = await Tour.find()
     //   .where("duration")
     //   .equals(5)
