e951ee2b934332200e6f3b67893838ab53ef1a75
oskarziembrowicz <rakson2002@gmail.com>
2025-03-15 13:22:04 +0100

    Implementing pagination to get all tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -3,19 +3,19 @@ const Tour = require("../models/tourModel");
 exports.getAllTours = async (req, res) => {
   try {
     // BUILD QUERY
-    // 1) Filtering
+    // 1a) Filtering
     const queryObj = { ...req.query };
     const excludedFields = ["page", "sort", "limit", "fields"];
     excludedFields.forEach((field) => delete queryObj[field]);
 
-    // 2) Advanced filering
+    // 1b) Advanced filering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
     // console.log(JSON.parse(queryStr));
 
     let query = Tour.find(JSON.parse(queryStr));
 
-    // 3) Sorting
+    // 2) Sorting
     if (req.query.sort) {
       const sortBy = req.query.sort.split(",").join(" ");
       query = query.sort(sortBy);
@@ -23,7 +23,7 @@ exports.getAllTours = async (req, res) => {
       query = query.sort("-createdAt");
     }
 
-    // 4) Field limiting
+    // 3) Field limiting
     if (req.query.fields) {
       const fields = req.query.fields.split(",").join(" ");
       query = query.select(fields);
@@ -31,16 +31,21 @@ exports.getAllTours = async (req, res) => {
       query = query.select("-__v");
     }
 
-    // const tours = await Tour.find()
-    //   .where("duration")
-    //   .equals(5)
-    //   .where("difficulty")
-    //   .equals("easy");
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = limit * (page - 1);
+    query = query.skip(skip).limit(limit);
+    // Check if the page is not empty
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error("This page does not exist");
+    }
 
     // EXECUTE QUERY
     const tours = await query;
 
-    // Send the response
+    // SEND THE RESPONSE
     res.status(200).json({
       status: "success",
       results: tours.length,
