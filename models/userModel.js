0d59098eb0fcfe88fda3004d7874316224a25750
oskarziembrowicz <rakson2002@gmail.com>
2025-03-21 14:33:27 +0100

    Hashing passwords

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,7 +23,26 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, "Please confirm youts password"],
+    validate: {
+      // This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: "Passwords are not the same",
+    },
   },
+});
+
+userSchema.pre("save", async function (next) {
+  // Only run if password was modified
+  if (!this.isModified("password")) return next();
+
+  // Hash with cost  of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model("User", userSchema);
