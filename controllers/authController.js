872cedb69c0e391233af0ca3b674a1f3a796cfcc
oskarziembrowicz <rakson2002@gmail.com>
2025-03-23 18:40:12 +0100

    Adding user roles and authorization for tour delete

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,6 +16,7 @@ exports.signup = catchAsync(async (req, res, next) => {
     password: req.body.password,
     passwordConfirm: req.body.passwordConfirm,
     passwordChangedAt: req.body.passwordChangedAt,
+    role: req.body.role,
   });
 
   const token = signToken(newUser._id);
@@ -88,3 +89,16 @@ exports.protect = catchAsync(async (req, res, next) => {
   req.user = currentUser;
   next();
 });
+
+exports.restrictTo =
+  (...roles) =>
+  (req, res, next) => {
+    // .protect() runs before, so req.user will be created already
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError("You do not have permission to do this action", 403),
+      );
+    }
+
+    next();
+  };
