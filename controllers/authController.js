ab1d30ee73ca1083a0fb861cca31f2bffdc70f13
oskarziembrowicz <rakson2002@gmail.com>
2025-04-03 14:46:36 +0200

    Sending the token via cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,19 @@ const signToken = (id) =>
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    // secure: true, // This should be true only in production (for https)
+    httpOnly: true, // This cannot be accessed or modified bu the browser
+  };
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
+
+  // Remove password from the output
+  user.password = undefined;
 
   res.status(statusCode).json({
     status: "success",
