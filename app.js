3ff6a4ce0ff71bcb80b12f327db6e88b936167fc
oskarziembrowicz <rakson2002@gmail.com>
2025-04-03 20:18:38 +0200

    Adding rate limit to the requests

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./controllers/errorController");
@@ -8,10 +9,19 @@ const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
-// 1. MIDDLEWARES
+// 1. GLOBAL MIDDLEWARES
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
+
+// This limiter is set to max of 100 request an hour
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many requests from this IP! Please try again in an hour.",
+});
+app.use("/api", limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
